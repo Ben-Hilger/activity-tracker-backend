d7a17e6a8c040e2f1e90853dd7f7a6786c2b8b73
Ben Hilger <bhilger78@gmail.com>
2024-03-08 23:36:26 -0500

    add db connection string value

--- src/main/java/com/benhilger/activitytracker/util/DatabaseAccess.java
+++ src/main/java/com/benhilger/activitytracker/util/DatabaseAccess.java
@@ -1,10 +1,17 @@
 package com.benhilger.activitytracker.util;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+@Component
 public class DatabaseAccess {
+
+    @Value("${SQL_DB_CONNECTION_STRING}")
+    private static String dbConnectionString;
 
     /**
      * Uses the configured environment variable to connect to the database
@@ -13,6 +20,6 @@ public class DatabaseAccess {
      * @throws SQLException if the database is unable to get connected to
      */
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("");
+        return DriverManager.getConnection(dbConnectionString);
     }
 }
